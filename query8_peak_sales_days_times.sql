9048dd012d924164e4dbb81d0332292552bf08af
ayesha_na <144324787+Ayesha-Nadeem7@users.noreply.github.com>
2025-05-31 19:07:13 +0500

    Delete query8_peak_sales_days_times.sql

--- query8_peak_sales_days_times.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-  FORMAT_TIMESTAMP('%Y-%m-%d', o.created_at) AS sale_date,
-  COUNT(o.order_id) AS total_orders,
-  EXTRACT(HOUR FROM o.created_at) AS sale_hour
-FROM `bigquery-public-data.thelook_ecommerce.orders` AS o
-GROUP BY sale_date, sale_hour
-ORDER BY sale_date DESC, sale_hour;
