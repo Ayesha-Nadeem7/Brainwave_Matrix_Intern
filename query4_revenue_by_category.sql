2d39a6a11548593335ddc4fc9cb5b4b4ba9b1133
ayesha_na <144324787+Ayesha-Nadeem7@users.noreply.github.com>
2025-05-31 19:06:22 +0500

    Delete query4_revenue_by_category.sql

--- query4_revenue_by_category.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-  p.category AS product_category,
-  ROUND(SUM(oi.sale_price), 2) AS total_revenue
-FROM `bigquery-public-data.thelook_ecommerce.order_items` AS oi
-JOIN `bigquery-public-data.thelook_ecommerce.products` AS p
-ON oi.product_id = p.id
-GROUP BY product_category
-ORDER BY total_revenue DESC;
