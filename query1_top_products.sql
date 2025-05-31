47651e9c2282f1c60ca7d02b97e3ee5dedeb99bf
ayesha_na <144324787+Ayesha-Nadeem7@users.noreply.github.com>
2025-05-31 18:59:57 +0500

    Delete query1_top_products.sql

--- query1_top_products.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-  p.name AS product_name,
-  SUM(oi.sale_price) AS total_revenue
-FROM `bigquery-public-data.thelook_ecommerce.order_items` AS oi
-JOIN `bigquery-public-data.thelook_ecommerce.products` AS p
-ON oi.product_id = p.id
-GROUP BY product_name
-ORDER BY total_revenue DESC
-LIMIT 10;
